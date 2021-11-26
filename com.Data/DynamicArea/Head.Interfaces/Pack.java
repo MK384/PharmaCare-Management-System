c3fe244b129a327321df0c46273d27ff3a84e35c
Mohammed Khaled <MK1668@fayoum.edu.eg>
2021-11-26 17:09:20 +0200

    I made some of main interfaces will be used in the whole data layer >> has to be continued.

---
+++ com.Data/DynamicArea/Head.Interfaces/Pack.java
@@ -0,0 +1,9 @@
+package DynamicArea.Head.Interfaces;
+
+/**
+ * this interface type holds all the plain info about a package of product.
+ */
+public interface Pack {
+
+
+}
---
+++ com.Data/DynamicArea/Head.Interfaces/ProductStock.java
@@ -0,0 +1,22 @@
+package DynamicArea.Head.Interfaces;
+
+/**
+ * this interface for an instances variables which hold all information about an existed product in the inventory.
+ */
+public interface ProductStock {
+
+
+public Product getProduct ();
+
+
+
+
+
+
+
+
+
+
+
+
+}
