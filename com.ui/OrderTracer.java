ee296ec3d833a4a3d97fa38b4042aa1d24a76e4b
AyaYasser1112002 <aya889142@gmail.com>
2022-01-05 16:07:52 +0200

    Completly edited and finished dynamic area

--- com.ui/OrderTracer.java
+++ com.ui/OrderTracer.java
@@ -1,4 +1,18 @@
 public class OrderTracer {
 
+    private final static int ORDER_DATE = "       Order Date       ".length();
+    private final static int DISCOUNT = "       Discount       ".length();
+    private final static int ITEMS_NUM = "       Items Number       ".length();
+    private final static int NOTE = "       Note       ".length();
+    private final static int CLIENT_NAME = "       Client Name       ".length();
+
+    private static void traceOrderHeader(){
+        System.out.println("");
+        System.out.println("");
+        System.out.println("                                                           |[ Order Details ]|                                                            ");
+        System.out.println("======================================================================================================================================");
+        System.out.println("|       Client Name     ||   Order Date   ||       Items Num        || Discount ||   Note   |");
+        System.out.println("======================================================================================================================================");
+    }
 
 }
