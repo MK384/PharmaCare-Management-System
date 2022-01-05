921a6c07f643d95c0ececff700cfea98fac8d98d
Fatma9251 <fatma.mohamed9251@gmail.com>
2022-01-05 17:00:18 +0200

    tracePack method in OrderTracer

--- com.ui/OrderTracer.java
+++ com.ui/OrderTracer.java
@@ -1,3 +1,7 @@
+import dynamic.area.Pack;
+import dynamic.area.head.ProductStock;
+import dynamic.area.head.orderImp;
+
 public class OrderTracer {
 
     private final static int ORDER_DATE = "                                ".length();
@@ -21,6 +25,15 @@ public class OrderTracer {
         System.out.println("======================================================================================================================================");
         System.out.println("|       Client Name     ||   Order Date   ||       Items Num        || Discount ||   Note   |");
         System.out.println("======================================================================================================================================");
+    }
+    private static void tracePack(Pack p){
+
+        System.out.println("|" + StockTracer.centerText(PRODUCT_NAME_SPACE,p.getProduct().getName()) + "|");
+        System.out.println("|"+ StockTracer.centerText(QUANTITY_SPACE,String.valueOf(p.getQuantity())) + "|" );
+        System.out.println("|"  + StockTracer.centerText(PROVIDER_SPACE,p.getProvider()) + "|"  );
+        System.out.println("|"  + StockTracer.centerText(LOAD_DATE_SPACE,p.getLoadDate().toLocaleString()) + "|");
+        System.out.println("|"  + StockTracer.centerText(EXP_DATE_SPACE,p.getExpDate().toLocaleString().substring(0,11)) + "|");
+
     }
 
 }
