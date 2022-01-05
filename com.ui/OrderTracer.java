28c78e70b367358d76cb400b1d9ef86a0307c43c
Mohammed Khaled <MK1668@fayoum.edu.eg>
2022-01-05 16:45:54 +0200

    works on UI

--- com.ui/OrderTracer.java
+++ com.ui/OrderTracer.java
@@ -1,10 +1,18 @@
 public class OrderTracer {
 
-    private final static int ORDER_DATE = "       Order Date       ".length();
-    private final static int DISCOUNT = "       Discount       ".length();
-    private final static int ITEMS_NUM = "       Items Number       ".length();
-    private final static int NOTE = "       Note       ".length();
-    private final static int CLIENT_NAME = "       Client Name       ".length();
+    private final static int ORDER_DATE = "                                ".length();
+    private final static int DISCOUNT = "                         ".length();
+    private final static int ITEMS_NUM = "                                     ".length();
+    private final static int TOTAL_CASH = "                                                           ".length();
+    private final static int CLIENT_NAME = "                            ".length();
+
+    private final static int PRODUCT_NAME_SPACE = "       Product Name     ".length();
+    private final static int EXP_DATE_SPACE = "            Expire Date          ".length();
+    private final static int LOAD_DATE_SPACE = "           Loading Date          ".length();
+    private final static int PROVIDER_SPACE = "    Merchant name    ".length();
+    private final static int QUANTITY_SPACE = "      Quantity      ".length();
+
+
 
     private static void traceOrderHeader(){
         System.out.println("");
